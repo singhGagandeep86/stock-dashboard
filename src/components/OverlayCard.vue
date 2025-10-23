8fbd29dd2323e612f2e62d6c41d5c63c769db000
GagandeepSingh <gagandeep.201119@gmail.com>
2025-10-23 14:52:11 +0200

    added new sheet & getting values

--- src/components/OverlayCard.vue
+++ src/components/OverlayCard.vue
@@ -1,60 +1,86 @@
 <template>
-<div class="overlay">
+  <div class="overlay">
 
-  <b-card v-for="(revenue, index) in arrayOfRevenue" :key="index" class="company-card">
-    <template v-for="(value, i) in revenue" :key="i">
-      <span v-if="i == revenue.length - 1">{{revenue[i]}}</span>
-        </template>
-      <!-- <span>{{ revenue.length }}</span> -->
-      {{ companies[index] }}
-    </b-card>   
-</div>
+    <b-card v-for="(revenue, index) in arrayOfRevenue" :key="index" class="company-card">
+     <div class="company-head"><img :src="companies[index].logo">  {{ companies[index].name }}</div>
+       <template v-for="(value, i) in revenue" :key="i">
+         <span v-if="i == revenue.length - 1">Revenue {{ lastQuarterOfCompnies[index][i] }}</span>
+        <span v-if="i == revenue.length - 1">{{ revenue[i] }}</span>
+      </template>
+    </b-card>
+  </div>
 </template>
 
 <script setup>
 import { ref, onMounted } from 'vue';
 import { stockService } from '@/services/stockService';
+import appleLogo from '@/assets/Images/logo/apple.png'
+import metaLogo from '@/assets/Images/logo/meta.png'
+import microsoftLogo from '@/assets/Images/logo/microsoft.png'
+import googleLogo from '@/assets/Images/logo/google.png'
+import amazonLogo from '@/assets/Images/logo/amazon.png'
+import teslaLogo from '@/assets/Images/logo/tesla.png'
+import nvidiaLogo from '@/assets/Images/logo/nvidia.png'
 let revenueOfCompanies = ref([]);
 let companies = ref([]);
 let arrayOfRevenue = ref([]);
+let lastQuarterOfCompnies = ref([]);
 
 onMounted(async () => {
-    const Apple = await stockService.fetchData('$AAPL');
-const Meta = await stockService.fetchData('$META');
-const Microsoft = await stockService.fetchData('$MSFT');
-const Google = await stockService.fetchData('$GOOG');
-const Amazon = await stockService.fetchData('$AMZN');
-const Tesla = await stockService.fetchData('$TSLA');
-const Nvidia = await stockService.fetchData('$NVDA');
+  const Apple = await stockService.fetchData('$AAPL');
+  const Meta = await stockService.fetchData('$META');
+  const Microsoft = await stockService.fetchData('$MSFT');
+  const Google = await stockService.fetchData('$GOOG');
+  const Amazon = await stockService.fetchData('$AMZN');
+  const Tesla = await stockService.fetchData('$TSLA');
+  const Nvidia = await stockService.fetchData('$NVDA');
 
- revenueOfCompanies.value = [Apple[5], Meta[5], Microsoft[9], Google[3], Amazon[9], Tesla[13], Nvidia[3]];
- arrayOfRevenue.value = revenueOfCompanies.value.map(item => Object.values(item));
- companies.value = ['Apple', 'Meta', 'Microsoft', 'Google', 'Amazon', 'Tesla', 'Nvidia'];
-    console.log('Loaded data', revenueOfCompanies.value, companies.value, arrayOfRevenue );
-    
+  revenueOfCompanies.value = [Apple[8], Meta[4], Microsoft[8], Google[4], Amazon[8], Tesla[13], Nvidia[4]];
+  lastQuarterOfCompnies.value = [Apple[2], Meta[2], Microsoft[2], Google[2], Amazon[2], Tesla[2], Nvidia[2]];
+  arrayOfRevenue.value = revenueOfCompanies.value.map(item => Object.values(item));
+  companies.value = [
+    { name: 'Apple', logo: appleLogo },
+    { name: 'Meta', logo: metaLogo },
+    { name: 'Microsoft', logo: microsoftLogo },
+    { name: 'Google', logo: googleLogo },
+    { name: 'Amazon', logo: amazonLogo },
+    { name: 'Tesla', logo: teslaLogo },
+    { name: 'Nvidia', logo: nvidiaLogo }
+  ];
+  console.log('Loaded data', revenueOfCompanies.value, companies.value, arrayOfRevenue);
+
 })
 </script>
 
 <style scoped>
-
 .overlay {
-    background: #023A6233;
-    display: flex;
-    flex-direction: row !important;
-    padding: 24px;
-    gap: 24px;
-    margin: 0px 100px;
-    border-radius: 16px;
-    color: white;
+  background: #023A6233;
+  display: flex;
+  flex-direction: row !important;
+  height: 190px;
+  padding: 24px;
+  gap: 24px;
+  margin: 0px 100px;
+  border-radius: 16px;
+  color: white;
 }
 
 .company-card {
-    width: 173px;
-    height: 100%;
-    border-radius: 16px;
-    background-color: #011F35;
-    padding: 20px 24px;
-    color: white;
+  /* width: 173px; */
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  border-radius: 16px;
+  background-color: #011F35;
+  /* padding: 20px 24px; */
+  color: white;
 }
 
+.company-head {
+  display: flex;
+  align-items: center;
+  gap: 8px;
+  font-size: 20px;
+  font-weight: 500;
+}
 </style>
--- src/services/stockService.js
+++ src/services/stockService.js
@@ -2,17 +2,22 @@ import axios from 'axios';
 
 class StockService {
   constructor() {
+    this.sheetId = '1m3h9Xce3SRMaq20li2Qg4HuSZowwxQSd9FkQmeuI7Dw';
+    this.apiKey = 'AIzaSyDN75qeS4l6XHygT8AIbylsyGIPQ-H2NN8';
+
     this.apiClient = axios.create({
-      baseURL: 'https://sheetdb.io/api/v1/ytxaav6pgnxbr'
+      baseURL: 'https://sheets.googleapis.com/v4/spreadsheets',
     });
   }
 
-  async fetchData(sheetName) {
+  async fetchData(sheetName = 'Sheet1') {
     try {
-      const response = await this.apiClient.get(`?sheet=${sheetName}`);
-      return response.data;
+      const response = await this.apiClient.get(
+        `/${this.sheetId}/values/${encodeURIComponent(sheetName)}?key=${this.apiKey}`
+      );
+      return response.data.values; 
     } catch (error) {
-      console.error('Error fetching data:', error);
+      console.error(' Fehler beim Laden der Daten:', error);
       return [];
     }
   }
